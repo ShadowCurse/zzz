6c66e81945cb02858ed5ec7be406c0058e685d7b
ShadowCurse <shadowcurse57@gmail.com>
2023-03-07 22:06:40 +0000

    first compile

--- src/syntax.zig
+++ src/syntax.zig
@@ -27,38 +27,29 @@ pub const EditorSyntax = struct {
 
 const C_HL_extensions = [_][]const u8{ ".c", ".h", ".cpp", ".hpp", ".cc" };
 const C_HL_keywords = [_][]const u8{
-    // C Keywords
-    "auto",          "break",       "case",      "continue",  "default",      "do",        "else",   "enum",
-    "extern",        "for",         "goto",      "if",        "register",     "return",    "sizeof", "static",
-    "struct",        "switch",      "typedef",   "union",     "volatile",     "while",     "NULL",
-
-    // C+=1 Keywords
-      "alignas",
-    "alignof",       "and",         "and_eq",    "asm",       "bitand",       "bitor",     "class",  "compl",
-    "constexpr",     "const_cast",  "deltype",   "delete",    "dynamic_cast", "explicit",  "export", "false",
-    "friend",        "inline",      "mutable",   "namespace", "new",          "noexcept",  "not",    "not_eq",
-    "nullptr",       "operator",    "or",        "or_eq",     "private",      "protected", "public", "reinterpret_cast",
-    "static_assert", "static_cast", "template",  "this",      "thread_local", "throw",     "true",   "try",
-    "typeid",        "typename",    "virtual",   "xor",       "xor_eq",
-
-    // C types
-          "int|",      "long|",  "double|",
-    "float|",        "char|",       "unsigned|", "signed|",   "void|",        "short|",    "auto|",  "const|",
-    "bool|",
+    "auto",          "break",       "case",     "continue",  "default",      "do",        "else",   "enum",
+    "extern",        "for",         "goto",     "if",        "register",     "return",    "sizeof", "static",
+    "struct",        "switch",      "typedef",  "union",     "volatile",     "while",     "NULL",   "alignas",
+    "alignof",       "and",         "and_eq",   "asm",       "bitand",       "bitor",     "class",  "compl",
+    "constexpr",     "const_cast",  "deltype",  "delete",    "dynamic_cast", "explicit",  "export", "false",
+    "friend",        "inline",      "mutable",  "namespace", "new",          "noexcept",  "not",    "not_eq",
+    "nullptr",       "operator",    "or",       "or_eq",     "private",      "protected", "public", "reinterpret_cast",
+    "static_assert", "static_cast", "template", "this",      "thread_local", "throw",     "true",   "try",
+    "typeid",        "typename",    "virtual",  "xor",       "xor_eq",       "int",       "long",   "double",
+    "float",         "char",        "unsigned", "signed",    "void",         "short",     "const",  "bool",
 };
 
 // Here we define an array of syntax highlights by extensions, keywords, comments delimiters and flags.
 pub const HLDB = [_]EditorSyntax{
-    // C / C++
-    EditorSyntax {
-        .extensions = &C_HL_extensions,
-        .keywords = &C_HL_keywords,
-        .singleline_comment_start = "//",
-        .multiline_comment_start = "/*",
-        .multiline_comment_end = "*/",
-        .flags = HL_HIGHLIGHT_STRINGS | HL_HIGHLIGHT_NUMBERS,
-    }
-};
+// C / C++
+EditorSyntax{
+    .extensions = &C_HL_extensions,
+    .keywords = &C_HL_keywords,
+    .singleline_comment_start = "//",
+    .multiline_comment_start = "/*",
+    .multiline_comment_end = "*/",
+    .flags = HL_HIGHLIGHT_STRINGS | HL_HIGHLIGHT_NUMBERS,
+}};
 
 // Maps syntax highlight token types to terminal colors.
 pub fn syntaxToColor(hl: i32) i32 {
